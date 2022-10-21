91d51242265ba3dad64d38405aa09bcfe3af16ad
Shalopay <shalopay@bk.ru>
2022-10-21 20:05:19 +0300

    Initial Commit

---
+++ FileManager/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FileManager
+//
+//  Created by Shalopay on 21.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
